1a7a61021cc052abe79561b81e457c170b581ca3
jLereback <112405931+jLereback@users.noreply.github.com>
2023-03-09 16:55:01 +0100

    Refactor directories
    Co-authored-by: JesperBodin <112554269+jesperbodin@users.noreply.github.com>
    Co-authored-by: Ahlberg-iths <113943951+Ahlberg-iths@users.noreply.github.com>

--- src/main/java/org/coppeloons/giveMeAProperName/DemoApplication.java
+++ src/main/java/org/coppeloons/giveMeAProperName/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package org.coppeloons.giveMeAProperName;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
