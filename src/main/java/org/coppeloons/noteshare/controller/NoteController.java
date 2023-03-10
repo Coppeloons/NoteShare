69f2b599f81a3b034b367fdf4e1cd9c303d5d7db
Stefan Karlsson <phtefan.karlsson@gmail.com>
2023-03-10 16:20:10 +0100

    Add getNote and getAllNotes to NoteController.java
    Co-authored-by: JesperBodin <112554269+jesperbodin@users.noreply.github.com>
    
    Co-authored-by: jLereback <112405931+jlereback@users.noreply.github.com>
    
    Co-authored-by: Ahlberg-iths <113943951+ahlberg-iths@users.noreply.github.com>
    
    Co-authored-by: Victorianilsson <120335470+victorianilsson@users.noreply.github.com>

--- src/main/java/org/coppeloons/noteshare/controller/NoteController.java
+++ src/main/java/org/coppeloons/noteshare/controller/NoteController.java
@@ -2,10 +2,9 @@ package org.coppeloons.noteshare.controller;
 
 import org.coppeloons.noteshare.entity.Note;
 import org.coppeloons.noteshare.repository.NoteRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/notes")
@@ -16,7 +15,13 @@ public class NoteController {
     void addNote(@RequestBody Note note){
         noteRepo.save(note);
     }
-
-
+    @GetMapping("/{id}")
+    Note getNote(@PathVariable Long id){
+        return noteRepo.findById(id).orElseThrow();
+    }
+    @GetMapping
+    List<Note> getAllNotes(){
+        return noteRepo.findAll();
+    }
 
 }
