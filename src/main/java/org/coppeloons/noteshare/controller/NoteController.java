373d43231b578d919ac9a10cb39c3782ea02af18
Stefan Karlsson <phtefan.karlsson@gmail.com>
2023-03-10 16:13:25 +0100

    Add addNote to NoteController.java
    Co-authored-by: JesperBodin <112554269+jesperbodin@users.noreply.github.com>
    
    Co-authored-by: jLereback <112405931+jlereback@users.noreply.github.com>
    
    Co-authored-by: Ahlberg-iths <113943951+ahlberg-iths@users.noreply.github.com>
    
    Co-authored-by: Victorianilsson <120335470+victorianilsson@users.noreply.github.com>

--- src/main/java/org/coppeloons/noteshare/controller/NoteController.java
+++ src/main/java/org/coppeloons/noteshare/controller/NoteController.java
@@ -1,4 +1,22 @@
 package org.coppeloons.noteshare.controller;
 
+import org.coppeloons.noteshare.entity.Note;
+import org.coppeloons.noteshare.repository.NoteRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notes")
 public class NoteController {
+
+    NoteRepository noteRepo;
+    @PostMapping
+    void addNote(@RequestBody Note note){
+        noteRepo.save(note);
+    }
+
+
+
 }
