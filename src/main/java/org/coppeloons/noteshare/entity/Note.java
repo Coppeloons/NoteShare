f69e62aef9ab22955bc634c24142944f207a7e1e
jLereback <112405931+jLereback@users.noreply.github.com>
2023-03-10 14:33:23 +0100

    Add Note.java
    Co-authored-by: Stefan Karlsson <phtefan.karlsson@gmail.com>
    
    Co-authored-by: Ahlberg-iths <113943951+Ahlberg-iths@users.noreply.github.com>
    
    Co-authored-by: VictoriaNilsson <120335470+VictoriaNilsson@users.noreply.github.com>

---
+++ src/main/java/org/coppeloons/noteshare/entity/Note.java
@@ -0,0 +1,38 @@
+package org.coppeloons.noteshare.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String documentName;
+
+    private String document;
+
+    @ManyToMany
+    private Set<User> users = new HashSet<>();
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Note note = (Note) o;
+        return id.equals(note.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
