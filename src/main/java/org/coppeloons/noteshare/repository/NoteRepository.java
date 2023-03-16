7a3eb4d5384c0595375d0e6a86775e61660c0f1b
Victorianilsson <120335470+VictoriaNilsson@users.noreply.github.com>
2023-03-16 15:05:44 +0100

    Add @Entitygraph to NoteRepository.java
    Co-authored-by: jLereback <112405931+jlereback@users.noreply.github.com>
    
    Co-authored-by: Stefan Karlsson <phtefan.karlsson@gmail.com>
    
    Co-authored-by: Ahlberg-iths <113943951+ahlberg-iths@users.noreply.github.com>

--- src/main/java/org/coppeloons/noteshare/repository/NoteRepository.java
+++ src/main/java/org/coppeloons/noteshare/repository/NoteRepository.java
@@ -1,7 +1,14 @@
 package org.coppeloons.noteshare.repository;
 
 import org.coppeloons.noteshare.entity.Note;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface NoteRepository extends ListCrudRepository<Note, Long> {
+
+    @Override
+    @EntityGraph(value = "Organization.persons")
+    List<Note> findAll();
 }
