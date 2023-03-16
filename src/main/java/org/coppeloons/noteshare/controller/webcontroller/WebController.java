ac55585c3e51aa369e3f75f4fb2c8cf8d667ea58
jLereback <112405931+jLereback@users.noreply.github.com>
2023-03-16 16:28:31 +0100

    Add users.html and add code to WebController.java
    Co-authored-by: Ahlberg-iths <113943951+ahlberg-iths@users.noreply.github.com>
    
    Co-authored-by: JesperBodin <112554269+jesperbodin@users.noreply.github.com>
    
    Co-authored-by: Stefan Karlsson <phtefan.karlsson@gmail.com>
    
    Co-authored-by: VictoriaNilsson <120335470+victorianilsson@users.noreply.github.com>

--- src/main/java/org/coppeloons/noteshare/controller/webcontroller/WebController.java
+++ src/main/java/org/coppeloons/noteshare/controller/webcontroller/WebController.java
@@ -1,4 +1,22 @@
 package org.coppeloons.noteshare.controller.webcontroller;
 
+import org.coppeloons.noteshare.repository.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class WebController {
+
+    private final UserRepository userRepo;
+
+    public WebController(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @GetMapping("/showUsers")
+    String users(Model model) {
+        model.addAttribute("allUsers", userRepo.findAll());
+        return "users";
+    }
 }
