50379414012a072f124a0eb27b031730e689b3f5
Ahlberg-iths <113943951+Ahlberg-iths@users.noreply.github.com>
2023-03-20 13:22:19 +0100

    Add GlobalControllerAdvice.java
    Co-authored-by: jLereback <112405931+jlereback@users.noreply.github.com>

---
+++ src/main/java/org/coppeloons/noteshare/controller/GlobalControllerAdvice.java
@@ -0,0 +1,22 @@
+package org.coppeloons.noteshare.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+@ResponseBody
+public class GlobalControllerAdvice {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(value = NoSuchElementException.class)
+    String handleNoSuchElementException(NoSuchElementException e) {
+
+        return "Resource not found - " + e.getMessage();
+    }
+
+}
