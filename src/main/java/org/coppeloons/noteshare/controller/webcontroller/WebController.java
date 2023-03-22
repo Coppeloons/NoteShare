dfe4b1783a1c3e2628646a559f44483ccd33c0ca
jLereback <112405931+jLereback@users.noreply.github.com>
2023-03-22 16:35:05 +0100

    Add note.html and user.html

--- src/main/java/org/coppeloons/noteshare/controller/webcontroller/WebController.java
+++ src/main/java/org/coppeloons/noteshare/controller/webcontroller/WebController.java
@@ -1,10 +1,15 @@
 package org.coppeloons.noteshare.controller.webcontroller;
 
+import org.coppeloons.noteshare.entity.Note;
 import org.coppeloons.noteshare.repository.NoteRepository;
 import org.coppeloons.noteshare.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class WebController {
@@ -23,9 +28,41 @@ public class WebController {
         return "users";
     }
 
+    @GetMapping("/viewUsers/{id}")
+    String user(Model model, @PathVariable Long id) {
+        model.addAttribute("user", userRepo.findById(id).orElseThrow());
+        return "user";
+    }
+
     @GetMapping("/viewNotes")
     String notes(Model model) {
         model.addAttribute("allNotes", noteRepo.findAll());
         return "notes";
+    }
+
+    @GetMapping("/viewNotes/{id}")
+    String note(Model model, @PathVariable Long id) {
+        model.addAttribute("note", noteRepo.findById(id).orElseThrow());
+        return "note";
+    }
+
+    @GetMapping("/viewNotes/user/{id}")
+    String noteByUser(Model model, @PathVariable Long id) {
+        var allNotes = noteRepo.findAll();
+        var user = userRepo.findById(id).orElseThrow();
+        List<Note> notesByUser = new ArrayList<>();
+        for (Note note : allNotes) {
+            if (note.getUsers().contains(user)) {
+                notesByUser.add(note);
+                model.addAttribute("allNotes", notesByUser);
+            }
+        }
+        return "notes";
+    }
+
+    @GetMapping("/viewUsers/note/{id}")
+    String userByNote(Model model, @PathVariable Long id) {
+        model.addAttribute("allUsers", noteRepo.findById(id).orElseThrow().getUsers());
+        return "users";
     }
 }
