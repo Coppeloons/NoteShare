9f3d4015bea0f539407354920901d62cbdfb00a8
jLereback <112405931+jLereback@users.noreply.github.com>
2023-04-02 22:30:23 +0200

    Add message queue and queueListener
    Co-authored-by: Ahlberg-iths <113943951+ahlberg-iths@users.noreply.github.com>
    
    Co-authored-by: JesperBodin <112554269+jesperbodin@users.noreply.github.com>

--- src/main/java/org/coppeloons/noteshare/controller/HubController.java
+++ src/main/java/org/coppeloons/noteshare/controller/HubController.java
@@ -3,12 +3,12 @@ package org.coppeloons.noteshare.controller;
 import org.coppeloons.noteshare.dto.HubDto;
 import org.coppeloons.noteshare.dto.HubMapper;
 import org.coppeloons.noteshare.repository.HubRepository;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.amqp.core.Queue;
-
 
 import java.util.List;
 
@@ -28,15 +28,19 @@ public class HubController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    void addNote(@RequestBody HubDto hubDto) {
-        System.out.println(mapper.map(hubDto).toString());
-        rabbit.convertAndSend("hubQueue", mapper.map(hubDto).toString());
-        //        hubRepo.save(mapper.map(hubDto));
+    void addNote(@RequestBody String hubDtoString) {
+        rabbit.convertAndSend("hubQueue", hubDtoString);
     }
 
     @Bean
     public Queue hubQueue() {
         return new Queue("hubQueue", false);
+    }
+
+    @RabbitListener(queues = "hubQueue")
+    public void listener(String hubDtoString) throws InterruptedException {
+        Thread.sleep(1000); //Only for demo purposes
+        hubRepo.save(mapper.map(hubDtoString));
     }
 
     @GetMapping
--- src/main/java/org/coppeloons/noteshare/dto/HubMapper.java
+++ src/main/java/org/coppeloons/noteshare/dto/HubMapper.java
@@ -4,6 +4,8 @@ import org.coppeloons.noteshare.document.Hub;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 public class HubMapper {
@@ -24,5 +26,28 @@ public class HubMapper {
         hub.setText(hubDto.getText());
         hub.setUsers(hubDto.getUsers());
         return hub;
+    }
+
+    public Hub map(String note) {
+        final String titleRegex = "\"title\":\"([^\"]+)\"";
+        final String textRegex = "\"text\":\"([^\"]+)\"";
+        final String usersRegex = "\"users\":\"([^\"]+)\"";
+        
+        final Matcher titelMatcher = getPattern(titleRegex).matcher(note);
+        final Matcher textMatcher = getPattern(textRegex).matcher(note);
+        final Matcher usersMatcher = getPattern(usersRegex).matcher(note);
+
+        Hub hub = new Hub();
+
+        if (titelMatcher.find() && textMatcher.find() && usersMatcher.find()) {
+            hub.setTitle(titelMatcher.group(1));
+            hub.setText(textMatcher.group(1));
+            hub.setUsers(usersMatcher.group(1));
+        }
+        return hub;
+    }
+
+    private static Pattern getPattern(String regex) {
+        return Pattern.compile(regex);
     }
 }
